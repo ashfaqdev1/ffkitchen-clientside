e658c3b762d46c0cc8759ac3f74dcd3ca077e69e
ashfaq <ashfaqjutt1000000@gmail.com>
2026-06-27 19:05:16 -0700

    change server

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://51.20.118.240:5000",
         changeOrigin: true,
       },
       watch: {
